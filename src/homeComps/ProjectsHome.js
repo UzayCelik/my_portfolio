c23a1310ecccf55acaf04999480f80059bf93b4c
Uzay Celik <uzaycelik@gmail.com>
2023-01-20 12:48:16 +0300

    nasa

--- src/homeComps/ProjectsHome.js
+++ src/homeComps/ProjectsHome.js
@@ -13,7 +13,7 @@ export default function ProjectsHome(){
                             <img className="project-item-home-img"  src={item.img} alt="res"/><br/>
                             <strong>{item.name}</strong><br/>
                             <a href={item.gitHubLink} style={{ textDecoration: 'none',color:"blue" }}>
-                                {item.name} projemi gitHub' da Görüntüleyebilirsiniz
+                                {item.name} projemi Görüntüleyebilirsiniz
                             </a>
                         </div>
                     )
